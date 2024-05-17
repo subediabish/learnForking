489f8dc8e3968e6e9a523f86c079832acb8f29f1
Abishkar Subedi <subediabishkar@outlook.com>
2024-05-17 01:49:30 +0000

    moved return sum outside of loop, now the code should work

--- something.js
+++ something.js
@@ -1,3 +1,5 @@
+// write a program to get the the sum of all the multiples of 3 or 5 below the number passed in
+
 function solution(number) {
     var sum = 0;
     var multiples = []
@@ -6,9 +8,10 @@ function solution(number) {
             multiples.push(i);
             sum += i;
         }
-        console.log(multiples);
+        
+    }
+    console.log(multiples);
         console.log(`Sum: ${sum}`)
         return sum;
-    }
 }
-solution(15);
+solution(15); //you can try passing different numbers like 10, 16,20, 33, 100 etc.
